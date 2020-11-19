0493bb49a8b7c66db8e51d1e81d7491d76912f12
John Loane <john.loane@dkit.ie>
2020-11-19 12:09:42 +0000

    Set up start of project
    Maven quickstart files

---
+++ src/main/java/com/dkit/sd2a/johnloane/App.java
@@ -0,0 +1,13 @@
+package com.dkit.sd2a.johnloane;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
